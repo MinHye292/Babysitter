6d2ab3156b156dc5ad16c2903bc4d312346b67b7
amy3452 <8547364@naver.com>
2021-08-24 10:18:40 +0900

    새로운 프로젝트 생성

---
+++ BabysitterService/src/main/java/cat/BabysitterServiceApplication.java
@@ -0,0 +1,13 @@
+package cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabysitterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabysitterServiceApplication.class, args);
+	}
+
+}
